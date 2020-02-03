db93cf6460528316798a3152c91b783dea55d3ce
taroj khan <tarujkhan@yahoo.com>
2020-02-03 20:13:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -26,7 +26,7 @@ end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   holiday_hash[:winter].each do |holiday, supplies|
-    holiday_hash{:
+    
     supplies << supply
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
