c2c02a6dc75b7462171d12c0aa8647ab14c3a9e0
taroj khan <tarujkhan@yahoo.com>
2020-02-03 20:53:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,7 @@ end
 
 def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
-    season
+    puts "#{season.capitalize}"
     holiday.each do |holiday, supply|
     supply
   end
